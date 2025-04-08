b4ac986fc83931318b72eed372a6f0924ef26468
Brett Mariani <brettmariani923@gmail.com>
2025-04-08 03:06:13 -0500

    first commit

---
+++ OpenWeatherMap_API/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using OpenWeatherMap_API;
+
+namespace OpenWeatherMapAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var weatherLink = new WeatherLink(client);
+
+            Console.WriteLine("Fetching weather data...");
+            Console.WriteLine(weatherLink.Weather());
+        }
+    }
+}
---
+++ OpenWeatherMap_API/WeatherLink.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatherMap_API
+{
+    public class WeatherLink
+    {
+        private HttpClient _client;
+
+        public WeatherLink(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Weather()
+        {
+            var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=Gurnee,US&units=imperial&appid=d6932d83f438050cdbfaffdafef0b5db";
+
+            var getWeather = _client.GetAsync(weatherURL).Result;
+
+            var data = JObject.Parse(getWeather.Content.ReadAsStringAsync().Result);
+
+            string city = data["name"].ToString();
+            string description = data["weather"][0]["description"].ToString();
+            string temperature = data["main"]["temp"].ToString();
+            string humidity = data["main"]["humidity"].ToString();
+            string windSpeed = data["wind"]["speed"].ToString();
+            string windDirection = data["wind"]["deg"].ToString();
+
+            string result = $"Weather Report for {city}\n" +
+                   new string('-', 27) + "\n" +
+                   $"Description : {description}\n" +
+                   $"Temperature : {temperature}°F\n" +
+                   $"Humidity    : {humidity}%\n" +
+                   $"Wind Speed  : {windSpeed} mph\n" +
+                   $"Wind Dir.   : {windDirection}°\n" +
+                   new string('-', 27);
+
+            return result;
+
+        }
+    }
+}
